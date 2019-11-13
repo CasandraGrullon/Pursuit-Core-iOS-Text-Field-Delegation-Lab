93f8b34aa1c8354523127ba81b4449c4d3d60820
Casandra <casandragrullon@pursuit.org>
2019-11-13 14:57:29 -0500

    created check answers function. turns correct guesses green, wrong red

--- TextTwistProject/TextTwistProject/ViewController.swift
+++ TextTwistProject/TextTwistProject/ViewController.swift
@@ -10,18 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var selectedWord = Word.getRandomWord()
+
+    
+    @IBOutlet var screen: UIView!
     @IBOutlet weak var randomWordLabel: UILabel!
     
     @IBOutlet weak var textField: UITextField!
-    
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        randomWordLabel.text = Word.getRandomWord().scrambled
+        randomWordLabel.text = selectedWord.scrambled
+        print(selectedWord)
+        textField.delegate = self
     }
 
+    func checkResults() {
+        let guessed = (textField.text ?? "").lowercased()
+        
+        let userInput = Set(guessed)
+        let correctAnswer = Set(selectedWord.unscrambled)
+        
+        if userInput.isSubset(of: correctAnswer) {
+            if guessed.lowercased() == selectedWord.unscrambled {
+            screen.backgroundColor = .green
+            }
+        } else {
+            screen.backgroundColor = .red
+        }
+        
+    }
 
     @IBAction func resetButton(_ sender: UIButton) {
     }
@@ -33,4 +51,24 @@ class ViewController: UIViewController {
     
     
 }
-
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        
+        
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        checkResults()
+        textField.resignFirstResponder()
+        return true
+    }
+}
